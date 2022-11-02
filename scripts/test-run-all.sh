a4b28008bff29985f074fd062c25aa206ef3c340
Anthony Corletti <anthony@rungalileo.io>
2022-11-02 12:50:38 -0400

    chore: ci for example notebooks (#10)
    * chore: ci for example notebooks
    
    * dq 0.7.3 will have the latest hf ner integration work
    
    * ner cleanup
    
    * remove unnecessary cells and validation from tf and torch ner nbs

---
+++ scripts/test-run-all.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -e 
+
+echo "Running examples/multi_label_text_classification/Multi_Label_Text_Classification_using_Pytorch_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/multi_label_text_classification/Multi_Label_Text_Classification_using_Pytorch_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/multi_label_text_classification/Multi_Label_Text_Classification_using_TensorFlow_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/multi_label_text_classification/Multi_Label_Text_Classification_using_TensorFlow_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/named_entity_recognition/Named_Entity_Recognition_with_Huggingface_Trainer_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/named_entity_recognition/Named_Entity_Recognition_with_Huggingface_Trainer_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/named_entity_recognition/Named_Entity_Recognition_with_Pytorch_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/named_entity_recognition/Named_Entity_Recognition_with_Pytorch_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/named_entity_recognition/Named_Entity_Recognition_with_SpaCy_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/named_entity_recognition/Named_Entity_Recognition_with_SpaCy_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/named_entity_recognition/Named_Entity_Recognition_with_Tensorflow_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/named_entity_recognition/Named_Entity_Recognition_with_Tensorflow_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/natural_language_inference/Natural_Language_Inference_using_Pytorch_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/natural_language_inference/Natural_Language_Inference_using_Pytorch_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/natural_language_inference/Natural_Language_Inference_using_TensorFlow_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/natural_language_inference/Natural_Language_Inference_using_TensorFlow_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/text_classification/Text_Classification_using_Huggingface_Trainer_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/text_classification/Text_Classification_using_Huggingface_Trainer_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/text_classification/Text_Classification_using_Keras_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/text_classification/Text_Classification_using_Keras_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/text_classification/Text_Classification_using_PyTorch_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/text_classification/Text_Classification_using_PyTorch_and_🔭_Galileo.ipynb
+echo "Done!\n"
+
+echo "Running examples/text_classification/Text_Classification_using_Tensorflow_and_🔭_Galileo.ipynb"
+MINIMIZE_FOR_CI=true jupyter nbconvert --to notebook --execute examples/text_classification/Text_Classification_using_Tensorflow_and_🔭_Galileo.ipynb
+echo "Done!\n"
