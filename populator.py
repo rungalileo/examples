63bcf1d53b085ea9cbcef04eb745fadbc2228bb3
Anthony Corletti <anthony@rungalileo.io>
2023-05-09 17:45:18 -0400

    sc-4717: root content populators (#37)
    * chore: sc-4717: root content populator runs
    
    * chore: sc-4717: root content populator runs
    
    * chore: sc-4717: root content populator runs
    
    * chore: sc-4717: root content populator runs
    
    * chore: sc-4717: root content populator runs
    
    * sc-4717: simple examples
    
    * sc-4717: simple examples
    
    * sc-4717: simple examples
    
    * sc-4717: test without GPU
    
    * update names
    
    * cache can stall gh runs
    
    * bump system requirements
    
    * bump system requirements and separate call w gpu for images
    
    * shrink this down for testing
    
    * shrink this down for testing
    
    * shrink this down for testing
    
    * shrink this down for testing
    
    * enable all
    
    * test full mltc
    
    * test full mltc without a gpu
    
    * test full mltc with a gpu, to(device) calls on hamming distance
    
    * test full tc, ner, nli, ic with gpus
    
    * test nli
    
    * extract the batch from the device
    
    * debug batch
    
    * debug batch
    
    * debug batch
    
    * debug batch
    
    * debug batch
    
    * enable all runs
    
    * set condition back

---
+++ populator.py
@@ -0,0 +1,74 @@
+"""populator
+
+This helper script is used to populate example run content that new Galileo users see in example runs after they sign up.
+
+To run this script, install `modal`
+
+```bash
+pip install modal-client
+```
+
+Then run the following command with the path to the notebook you want to run
+
+```bash
+modal run populator.py --notebook-path <path-to-notebook>
+```
+"""
+
+import os
+
+from modal import Image, Mount, Secret, Stub, gpu
+
+image = (
+    Image.debian_slim()
+    .env({"MINIMIZE_FOR_CI": os.getenv("MINIMIZE_FOR_CI", "false")})
+    .pip_install_from_requirements(requirements_txt="requirements-ci.txt")
+)
+stub = Stub(
+    name="populator",
+    image=image,
+)
+secret = Secret.from_name("examples-populator-secret")
+
+
+def _modalinclude(path: str) -> bool:
+    return path.endswith(".ipynb") and "checkpoint" not in path
+
+
+mounts = [
+    Mount.from_local_dir(
+        local_path="examples",
+        remote_path="/root/examples",
+        condition=_modalinclude,
+    )
+]
+
+
+def _run(path: str) -> None:
+    import subprocess
+
+    cmd = f"jupyter nbconvert --debug --to notebook --execute {path}"
+    subprocess.run(cmd, shell=True)
+
+
+@stub.function(
+    secret=secret,
+    mounts=mounts,
+    cpu=4,
+    memory=4096,
+    timeout=60 * 60 * 5,
+)
+def run(notebook_path: str) -> None:
+    _run(notebook_path)
+
+
+@stub.function(
+    secret=secret,
+    mounts=mounts,
+    cpu=4,
+    memory=4096,
+    timeout=60 * 60 * 5,
+    gpu=gpu.T4(),
+)
+def run_gpu(notebook_path: str) -> None:
+    _run(notebook_path)
